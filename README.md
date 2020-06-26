8d4dd80e66b26d57462d3f001f2738dbce5cdcd7
Schalk Burger <schalk.burger@kurtosys.com>
2020-06-26 07:45:43 +0200

    Initial styling of Google Calendar Dark

---
+++ README.md
@@ -0,0 +1,5 @@
+# Google Calendar Dark
+
+A dark theme for Google Calendar
+
+<!-- ![Image of Hacker News Readable Dark](hacker-news-readable-dark-screenshot.png) -->
---
+++ google-calendar-dark.user.js
@@ -0,0 +1,22 @@
+@-moz-document url-prefix("https://news.ycombinator.com/") {
+
+    /* Globals  */
+
+    body {
+        background-color: #202124;
+        color: #e8eaed;
+        direction: ltr;
+        font-family: 'Roboto', arial, sans-serif;
+        font-size: 15px;
+        margin: 0;
+        min-width: 360px;
+        overflow-x: hidden;
+        overflow-y: auto;
+        padding: 0;
+    }
+
+    header {
+        background-color: #202124 !important;
+    }
+
+}
