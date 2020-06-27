0e9f9d6918eaacc0cda724f1b776cea47dca8af1
Schalk Burger <schalk.burger@kurtosys.com>
2020-06-27 19:23:39 +0200

    Update google-calendar-dark.user.js

--- google-calendar-dark.user.js
+++ google-calendar-dark.user.js
@@ -1,7 +1,6 @@
 @-moz-document url-prefix("https://calendar.google.com/calendar/") {
 
     /* Globals  */
-
     body {
         background-color: #21252c;
         color: #e8eaed;
@@ -16,25 +15,48 @@
     }
 
     /* Header */
-
     header {
         background-color: #21252c !important;
     }
 
-    body[data-viewfamily=EVENT] .SGWAac, body[data-viewfamily=SCHEDULE] .SGWAac, body[data-viewfamily=SEARCH] .SGWAac, body[data-viewfamily=SETTINGS] .SGWAac, body[data-viewfamily=TRASH] .SGWAac {
+    body[data-viewfamily=EVENT] .SGWAac,
+    body[data-viewfamily=SCHEDULE] .SGWAac,
+    body[data-viewfamily=SEARCH] .SGWAac,
+    body[data-viewfamily=SETTINGS] .SGWAac,
+    body[data-viewfamily=TRASH] .SGWAac {
         border: none !important;
     }
 
     /* Text colours */
+    .gb_sa svg,
+    .gb_Fc svg,
+    .gb_8c .gb_le,
+    .gb_Rc .gb_le,
+    .GXlaye .snByac,
+    div.rF3YF svg,
+    div.rF3YF span,
+    div.rF3YF svg,
+    div.rF3YF span,
+    .rSoRzd,
+    .HLI7qf .c7fp5b,
+    .OWyNBb.m1fiJb,
+    .uQ1ixe .snByac,
+    .Cd9hpd .snByac,
+    .u5sQsb,
+    .r4nke,
+    .NI2kfb, .qAeuG.F262Ye {
+        color: rgba(255, 255, 255, .87) !important;
+        fill: rgba(255, 255, 255, .87) !important;
+    }
 
-    .gb_sa svg, .gb_Fc svg, .gb_8c .gb_le, .gb_Rc .gb_le, .GXlaye .snByac, div.rF3YF svg, div.rF3YF span, div.rF3YF svg, div.rF3YF span, .rSoRzd, .HLI7qf .c7fp5b, .OWyNBb.m1fiJb, .uQ1ixe .snByac, .Cd9hpd .snByac, .u5sQsb, .r4nke {
-        color: rgba(255,255,255, .87 ) !important;
-        fill: rgba(255,255,255, .87 ) !important;
+    .MmhHI.N4XV7d {
+        color: #9d9d9d;
     }
 
     /* Calendar Sidebar */
-
-    .OWyNBb.m1fiJb, .W0m3G .r4nke, .W0m3G::before {
+    .OWyNBb.m1fiJb,
+    .W0m3G .r4nke,
+    .W0m3G::before {
         background-color: transparent !important;
     }
 
@@ -47,34 +69,75 @@
         color: #fff !important;
     }
 
-    .IIB0tc .ULpymb {
+    .IIB0tc .ULpymb,
+    .J09ahd {
         background-color: #282c35;
     }
 
     /* Calendar Main */
-
-    .v83gc, .GENA3c, .Gk2izd, ::-webkit-scrollbar-thumb {
+    .v83gc,
+    .GENA3c,
+    .Gk2izd {
         background-color: #282c35;
     }
 
-    .YvjgZe, .CIFHgc, .MVMVEe, .jRJqje::-webkit-scrollbar-thumb {
+    .YvjgZe,
+    .CIFHgc,
+    .MVMVEe,
+    .jRJqje::-webkit-scrollbar-thumb, .eADW5d {
         border-color: #4c4c4c !important;
     }
 
     .hnGhZ::-webkit-scrollbar-thumb {
-        border-bottom: 1px solid white;
-        border-left: 1px solid white;
-        border-right: 1px solid white;
-        border-top: 1px solid white;
+        border-bottom: 1px solid #dadce0;
+        border-left: 1px solid #dadce0;
+        border-right: 1px solid #dadce0;
+        border-top: 1px solid #dadce0;
     }
 
-    /* .j0nwNb .eh5oYe .eADW5d {
-        background-color: #101114;
-    } */
+    ::-webkit-scrollbar-thumb {
+        background: #1f1f1f;
+    }
+
+    ::-webkit-scrollbar {
+        background: #22262c;
+        height: 8px;
+        width: 8px;
+    }
+
+    .J2aUD.T8M5bd .sx5BGe::before, .SGFqbe.T8M5bd .sx5BGe::before {
+        background: none;
+    }
+
+    .KSxb4d.F262Ye, body.geSgge .KSxb4d.F262Ye, .eADW5d:first-child {
+        background: #212326 !important;
+    }
+
+    .g3dbUc.jKgTF.JRw8kf.QGRmIf, .g3dbUc.jKgTF.QGRmIf, .NlL62b.EfQccc.elYzab-cXXICe-Hjleke.EiZ8Dd.afiDFd {
+        background-color: #01579b !important;
+        border-bottom: 1px solid #525252;
+    }
+
+    .zWcBU.hyd2Ge.gpOGPc, .zWcBU.BvHyo.gpOGPc  {
+        border-color: #555454 !important;
+    }
+
+    .NlL62b.EfQccc.elYzab-cXXICe-Hjleke.EiZ8Dd.F262Ye, .NlL62b.EfQccc.elYzab-cXXICe-Hjleke.EiZ8Dd {
+        background: #004d40 !important;
+    }
+
+    body.geSgge .zWcBU.hyd2Ge {
+        border-right-width: 2px;
+    }
+
+    body.geSgge .zWcBU.BvHyo {
+        border-left-width: 2px;
+    }
 
     /* Far Right Sidebar */
-
-    body[data-viewfamily=EVENT] .gboEAb, body[data-viewfamily=SEARCH] .gboEAb, body[data-viewfamily=EVENT_EDIT] .gboEAb {
+    body[data-viewfamily=EVENT] .gboEAb,
+    body[data-viewfamily=SEARCH] .gboEAb,
+    body[data-viewfamily=EVENT_EDIT] .gboEAb {
         display: none;
     }
 
