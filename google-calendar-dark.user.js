421f1286d00b50d697dcc1a01f0af8ad32069d2b
Schalk Burger <schalk.burger@kurtosys.com>
2020-06-27 08:05:17 +0200

    Update google-calendar-dark.user.js

--- google-calendar-dark.user.js
+++ google-calendar-dark.user.js
@@ -1,9 +1,9 @@
-@-moz-document url-prefix("https://news.ycombinator.com/") {
+@-moz-document url-prefix("https://calendar.google.com/calendar/") {
 
     /* Globals  */
 
     body {
-        background-color: #202124;
+        background-color: #21252c;
         color: #e8eaed;
         direction: ltr;
         font-family: 'Roboto', arial, sans-serif;
@@ -15,8 +15,67 @@
         padding: 0;
     }
 
+    /* Header */
+
     header {
-        background-color: #202124 !important;
+        background-color: #21252c !important;
+    }
+
+    body[data-viewfamily=EVENT] .SGWAac, body[data-viewfamily=SCHEDULE] .SGWAac, body[data-viewfamily=SEARCH] .SGWAac, body[data-viewfamily=SETTINGS] .SGWAac, body[data-viewfamily=TRASH] .SGWAac {
+        border: none !important;
+    }
+
+    /* Text colours */
+
+    .gb_sa svg, .gb_Fc svg, .gb_8c .gb_le, .gb_Rc .gb_le, .GXlaye .snByac, div.rF3YF svg, div.rF3YF span, div.rF3YF svg, div.rF3YF span, .rSoRzd, .HLI7qf .c7fp5b, .OWyNBb.m1fiJb, .uQ1ixe .snByac, .Cd9hpd .snByac, .u5sQsb, .r4nke {
+        color: rgba(255,255,255, .87 ) !important;
+        fill: rgba(255,255,255, .87 ) !important;
+    }
+
+    /* Calendar Sidebar */
+
+    .OWyNBb.m1fiJb, .W0m3G .r4nke, .W0m3G::before {
+        background-color: transparent !important;
+    }
+
+    .hEtGGf::after {
+        background-image: none !important;
+    }
+
+    .SaBhMc {
+        background-color: #282c35;
+        color: #fff !important;
+    }
+
+    .IIB0tc .ULpymb {
+        background-color: #282c35;
+    }
+
+    /* Calendar Main */
+
+    .v83gc, .GENA3c, .Gk2izd, ::-webkit-scrollbar-thumb {
+        background-color: #282c35;
+    }
+
+    .YvjgZe, .CIFHgc, .MVMVEe, .jRJqje::-webkit-scrollbar-thumb {
+        border-color: #4c4c4c !important;
+    }
+
+    .hnGhZ::-webkit-scrollbar-thumb {
+        border-bottom: 1px solid white;
+        border-left: 1px solid white;
+        border-right: 1px solid white;
+        border-top: 1px solid white;
+    }
+
+    /* .j0nwNb .eh5oYe .eADW5d {
+        background-color: #101114;
+    } */
+
+    /* Far Right Sidebar */
+
+    body[data-viewfamily=EVENT] .gboEAb, body[data-viewfamily=SEARCH] .gboEAb, body[data-viewfamily=EVENT_EDIT] .gboEAb {
+        display: none;
     }
 
 }
