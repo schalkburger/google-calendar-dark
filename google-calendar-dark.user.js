3d2c550790101e9590e759618bbf85975157c011
Schalk Burger <schalkb@gmail.com>
2020-12-30 18:29:13 +0200

    Add styling for more elements

--- google-calendar-dark.user.js
+++ google-calendar-dark.user.js
@@ -1,192 +1,213 @@
 @-moz-document url-prefix("https://calendar.google.com/calendar/") {
-    /* Globals  */
-        body {
-            background-color: #21252c;
-            color: #e8eaed;
-            direction: ltr;
-            font-family: 'Roboto', arial, sans-serif;
-            font-size: 15px;
-            margin: 0;
-            min-width: 360px;
-            overflow-x: hidden;
-            overflow-y: auto;
-            padding: 0;
-        }
+    /* Globals  */  
     
-        /* Header */
-        header {
-            background-color: #21252c !important;
-        }
+    body {
+                background-color: #21252c;
+                color: #e8eaed;
+                direction: ltr;
+                font-family: 'Roboto', arial, sans-serif;
+                font-size: 15px;
+                margin: 0;
+                min-width: 360px;
+                overflow-x: hidden;
+                overflow-y: auto;
+                padding: 0;
+            }
     
-        body[data-viewfamily=EVENT] .SGWAac,
-        body[data-viewfamily=SCHEDULE] .SGWAac,
-        body[data-viewfamily=SEARCH] .SGWAac,
-        body[data-viewfamily=SETTINGS] .SGWAac,
-        body[data-viewfamily=TRASH] .SGWAac {
-            border: none !important;
-        }
+    [role="grid"] {
+        background: transparent !important;
+    }
     
-        /* Text colours */
-        .gb_sa svg,
-        .gb_Fc svg,
-        .gb_8c .gb_le,
-        .gb_Rc .gb_le,
-        .GXlaye .snByac,
-        div.rF3YF svg,
-        div.rF3YF span,
-        div.rF3YF svg,
-        div.rF3YF span,
-        .rSoRzd,
-        .HLI7qf .c7fp5b,
-        .OWyNBb.m1fiJb,
-        .uQ1ixe .snByac,
-        .Cd9hpd .snByac,
-        .u5sQsb,
-        .r4nke,
-        .NI2kfb, .qAeuG.F262Ye {
-            color: rgba(255, 255, 255, .87) !important;
-            fill: rgba(255, 255, 255, .87) !important;
-        }
+    /* Buttons */
     
-        .MmhHI.N4XV7d {
-            color: #9d9d9d;
-        }
+    [role="button"]:hover, [aria-expanded="true"] {
+        background: rgb(40 44 53) !important;
+    }
     
-        .J2aUD.T8M5bd .qmFsL::after, .SGFqbe.T8M5bd .qmFsL::after {
-            background-image: linear-gradient(to left,#282c35,rgb(40 44 53));
-        }
+    /* Dropdown Menu */
     
-        body.geSgge .g3dbUc {
-            color: #fff;
-        }
+    [role="menu"], [role="menu"] * {
+        background-color: #21252c !important;
+        color: #fff !important;
+    }
     
-        /* Calendar Sidebar */
+    /* Scrollbars */
     
-        .OWyNBb.m1fiJb,
-        .W0m3G .r4nke,
-        .W0m3G::before {
-            background-color: #282c35 !important;
-        }
-    
-        .hEtGGf::after {
-            background-image: none !important;
-        }
-    
-        .SaBhMc {
-            background-color: #282c35;
-            color: #fff !important;
-        }
-    
-        .IIB0tc .ULpymb,
-        .J09ahd {
-            background-color: #282c35;
-        }
-    
-        .OWyNBb.m1fiJb.e6o4df, .OWyNBb.m1fiJb.TbFp5e, .OWyNBb.m1fiJb {
-            border-color: #282c35;
-        }
-    
-        /* Calendar Main */
-        .v83gc,
-        .GENA3c,
-        .Gk2izd, .tNDBE {
-            background-color: #282c35;
-        }
-    
-        /* Week Row Left of Main */
-        .akJTWe, .XnnJrc.Wyo4Qe .TKMrfe {
-            background-color: #282c35;
-        }
-    
-        /* Day Numbers */ 
-    
-        div[role="presentation"] h2 {
-            color: #bbb !important;
-        }
-    
-        /* Current Day */
-    
-        div.t8qpF.elYzab-cXXICe-Hjleke.YK7obe.ef2wWc {
-            background-color: #2C3B51;
-        }
-    
-        div.t8qpF.elYzab-cXXICe-Hjleke.YK7obe.ef2wWc h2, .g3dbUc.smECzc {
-            color: #fff !important;
-        }
-    
-        .PhLhOd, .t8qpF, .QIadxc {
-            border-color: #21252c !important;
-            border-width: 8px;
-        }
-    
-        .YvjgZe,
-        .CIFHgc,
-        .MVMVEe,
-        .jRJqje::-webkit-scrollbar-thumb, .eADW5d {
-            border-color: #21252c !important;
-        }
-    
-        .hnGhZ::-webkit-scrollbar-thumb {
-            border-bottom: 1px solid #dadce0;
-            border-left: 1px solid #dadce0;
-            border-right: 1px solid #dadce0;
-            border-top: 1px solid #dadce0;
-        }
-    
-        ::-webkit-scrollbar-thumb {
-            background: #1f1f1f;
-        }
-    
-        ::-webkit-scrollbar {
-            background: #22262c;
-            height: 8px;
-            width: 8px;
-        }
-    
-        .J2aUD.T8M5bd .sx5BGe::before, .SGFqbe.T8M5bd .sx5BGe::before {
-            background: none;
-        }
-    
-        .KSxb4d.F262Ye, body.geSgge .KSxb4d.F262Ye {
-            background: #212326 !important;
-        }
-    
-        .g3dbUc.jKgTF.JRw8kf.QGRmIf, .g3dbUc.jKgTF.QGRmIf, .NlL62b.EfQccc.elYzab-cXXICe-Hjleke.EiZ8Dd.afiDFd {
-            background-color: #01579b !important;
-            border-bottom: 1px solid #525252;
-        }
-    
-        .zWcBU.hyd2Ge.gpOGPc, .zWcBU.BvHyo.gpOGPc  {
-            border-color: #555454 !important;
-        }
-    
-        .NlL62b.EfQccc.elYzab-cXXICe-Hjleke.EiZ8Dd.F262Ye, .NlL62b.EfQccc.elYzab-cXXICe-Hjleke.EiZ8Dd {
-            background: #004d40 !important;
-        }
-    
-        body.geSgge .zWcBU.hyd2Ge {
-            border-right-width: 2px;
-        }
-    
-        body.geSgge .zWcBU.BvHyo {
-            border-left-width: 2px;
-        }
-    
-        .Jmftzc.RIOtYe.PKhkGc, .g3dbUc.UflSff {
-            color: #fff;
-        }
-    
-        /* Far Right Sidebar */
-        body[data-viewfamily=EVENT] .gboEAb,
-        body[data-viewfamily=SEARCH] .gboEAb,
-        body[data-viewfamily=EVENT_EDIT] .gboEAb {
-            display: none;
-        }
-    
-        /* Create Event Modal */
-    
-        [data-layout-mode="bubble"] [jsslot], [data-layout-mode="bubble"] [data-dragsource-type="15"], [data-key="startDate"], [data-layout-mode="bubble"] span, [data-layout-mode="bubble"] [jsname] {
-            background: #464B53;
-            color: #fff !important;
-        }
+            .YvjgZe,
+            .CIFHgc,
+            .MVMVEe,
+            .jRJqje::-webkit-scrollbar-thumb, .eADW5d {
+                border-color: #21252c !important;
+            }
+        
+            .hnGhZ::-webkit-scrollbar-thumb {
+                border-bottom: 1px solid #dadce0;
+                border-left: 1px solid #dadce0;
+                border-right: 1px solid #dadce0;
+                border-top: 1px solid #dadce0;
+            }
+        
+            ::-webkit-scrollbar-thumb {
+                background: #dadce0;
+            }
+        
+            ::-webkit-scrollbar {
+                background: transparent;
+                height: 8px;
+                width: 8px;
+            }
+        
+            /* Header */
+            header {
+                background-color: #21252c !important;
+            }
+        
+            body[data-viewfamily=EVENT] .SGWAac,
+            body[data-viewfamily=SCHEDULE] .SGWAac,
+            body[data-viewfamily=SEARCH] .SGWAac,
+            body[data-viewfamily=SETTINGS] .SGWAac,
+            body[data-viewfamily=TRASH] .SGWAac {
+                border: none !important;
+            }
+        
+            /* Text colours */
+            .gb_sa svg,
+            .gb_Fc svg,
+            .gb_8c .gb_le,
+            .gb_Rc .gb_le,
+            .GXlaye .snByac,
+            div.rF3YF svg,
+            div.rF3YF span,
+            div.rF3YF svg,
+            div.rF3YF span,
+            .rSoRzd,
+            .HLI7qf .c7fp5b,
+            .OWyNBb.m1fiJb,
+            .uQ1ixe .snByac,
+            .Cd9hpd .snByac,
+            .u5sQsb,
+            .r4nke,
+            .NI2kfb, .qAeuG.F262Ye {
+                color: rgba(255, 255, 255, .87) !important;
+                fill: rgba(255, 255, 255, .87) !important;
+            }
+        
+            .MmhHI.N4XV7d {
+                color: #9d9d9d;
+            }
+        
+            .J2aUD.T8M5bd .qmFsL::after, .SGFqbe.T8M5bd .qmFsL::after {
+                background-image: linear-gradient(to left,#282c35,rgb(40 44 53));
+            }
+        
+            body.geSgge .g3dbUc {
+                color: #fff;
+            }
+        
+            /* Calendar Sidebar */
+        
+            .OWyNBb.m1fiJb,
+            .W0m3G .r4nke,
+            .W0m3G::before {
+                background-color: #282c35 !important;
+            }
+        
+            .hEtGGf::after {
+                background-image: none !important;
+            }
+        
+            .SaBhMc {
+                background-color: #282c35;
+                color: #fff !important;
+            }
+        
+            .IIB0tc .ULpymb,
+            .J09ahd {
+                background-color: #282c35;
+            }
+        
+            .OWyNBb.m1fiJb.e6o4df, .OWyNBb.m1fiJb.TbFp5e, .OWyNBb.m1fiJb {
+                border-color: #282c35;
+            }
+        
+            /* Calendar Main */
+            .v83gc,
+            .GENA3c,
+            .Gk2izd, .tNDBE {
+                background-color: #282c35;
+            }
+        
+            /* Week Row Left of Main */
+            .akJTWe, .XnnJrc.Wyo4Qe .TKMrfe {
+                background-color: #282c35;
+            }
+        
+            /* Day Numbers */ 
+        
+            div[role="presentation"] h2 {
+                color: #bbb !important;
+            }
+        
+            /* Current Day */
+        
+            div.t8qpF.elYzab-cXXICe-Hjleke.YK7obe.ef2wWc {
+                background-color: #2C3B51;
+            }
+        
+            div.t8qpF.elYzab-cXXICe-Hjleke.YK7obe.ef2wWc h2, .g3dbUc.smECzc {
+                color: #fff !important;
+            }
+        
+            .PhLhOd, .t8qpF, .QIadxc {
+                border-color: #21252c !important;
+                border-width: 8px;
+            }
+        
+        
+            .J2aUD.T8M5bd .sx5BGe::before, .SGFqbe.T8M5bd .sx5BGe::before {
+                background: none;
+            }
+        
+            .KSxb4d.F262Ye, body.geSgge .KSxb4d.F262Ye {
+                background: #212326 !important;
+            }
+        
+            .g3dbUc.jKgTF.JRw8kf.QGRmIf, .g3dbUc.jKgTF.QGRmIf, .NlL62b.EfQccc.elYzab-cXXICe-Hjleke.EiZ8Dd.afiDFd {
+                background-color: #01579b !important;
+                border-bottom: 1px solid #525252;
+            }
+        
+            .zWcBU.hyd2Ge.gpOGPc, .zWcBU.BvHyo.gpOGPc  {
+                border-color: #555454 !important;
+            }
+        
+            .NlL62b.EfQccc.elYzab-cXXICe-Hjleke.EiZ8Dd.F262Ye, .NlL62b.EfQccc.elYzab-cXXICe-Hjleke.EiZ8Dd {
+                background: #004d40 !important;
+            }
+        
+            body.geSgge .zWcBU.hyd2Ge {
+                border-right-width: 2px;
+            }
+        
+            body.geSgge .zWcBU.BvHyo {
+                border-left-width: 2px;
+            }
+        
+            .Jmftzc.RIOtYe.PKhkGc, .g3dbUc.UflSff {
+                color: #fff;
+            }
+        
+            /* Far Right Sidebar */
+            body[data-viewfamily=EVENT] .gboEAb,
+            body[data-viewfamily=SEARCH] .gboEAb,
+            body[data-viewfamily=EVENT_EDIT] .gboEAb {
+                display: none;
+            }
+        
+            /* Create Event Modal */
+        
+            [data-layout-mode="bubble"] [jsslot], [data-layout-mode="bubble"] [data-dragsource-type="15"], [data-key="startDate"], [data-layout-mode="bubble"] span, [data-layout-mode="bubble"] [jsname] {
+                background: #464B53 !important;
+                color: #fff !important;
+            }
     }
